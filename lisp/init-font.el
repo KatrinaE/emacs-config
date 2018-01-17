406b6f76ed4b60804653abecc17fc4ad89c4a666
Katrina Ellison Geltman <kgeltman@digitalocean.com>
2018-01-17 12:24:28 -0500

    disable linux fonts

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -11,24 +11,25 @@
 ;;     (set-default-font preferred-font)
 ;;   (set-default-font "DejaVu Sans Mono"))
 
-(defvar preferred-fonts '("Fira Mono"
-                          "Source Code Pro"
-                          "DejaVu Sans Mono"
-						  "Monaco"
-						  "Ubuntu Mono"
-						  "Hack"))
+;; (defvar preferred-fonts '("Fira Mono"
+;;                           "Source Code Pro"
+;;                           "DejaVu Sans Mono"
+;; 						  "Monaco"
+;; 						  "Ubuntu Mono"
+;; 						  "Hack"))
+;; (defvar preferred-fonts '("Noto Sans Mono"))
 
 ;; Set font for linux and misc.
-(if (eq system-type 'gnu/linux)
-	(if (display-graphic-p)
-		(if (font-existsp "Ubuntu Mono")
-			(set-frame-font "Ubuntu Mono" nil t)
-		  (set-frame-font "Monaco" nil t))
-	  (set-frame-font "Source Code Pro"))
-  )
+;; (if (eq system-type 'gnu/linux)
+;; 	(if (display-graphic-p)
+;; 		(if (font-existsp "Ubuntu Mono")
+;; 			(set-frame-font "Ubuntu Mono" nil t)
+;; 		  (set-frame-font "Monaco" nil t))
+;; 	  (set-frame-font "Source Code Pro"))
+;;   )
 
-(set-frame-font "Source Code Pro for Powerline" nil t)
+;; (set-frame-font "Source Code Pro for Powerline" nil t)
 
-(set-face-attribute 'default nil :height 100)
+(set-face-attribute 'default nil :height 150)
 
 (provide 'init-font)
